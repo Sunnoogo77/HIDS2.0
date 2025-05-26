4cc90ba650f03baa580c1f950ef8cd64ecdec1cd
Sunnoogo77 <sunnoogo-caleb.tapsoba@efrei.net>
2025-05-26 12:59:28 +0200

    26-05

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -3,9 +3,20 @@ from typing import Optional
 from jose import JWTError, jwt
 from app.core.config import settings
 
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from app.core.config import settings
+# from app.services.auth_service import  decode_access_token
+from app.db.models import User as ORMUser
+from app.db.session import SessionLocal
+
 #JWT constant
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30 #delais d'expiration du token
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
+
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
     """
@@ -20,8 +31,8 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -
     to_encode = data.copy()
     expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET, algorithm=ALGORITHM)
-    return encoded_jwt
+    return jwt.encode(to_encode, settings.JWT_SECRET, algorithm=ALGORITHM)
+
 
 def decode_access_token(token: str) -> dict:
     """
@@ -32,7 +43,23 @@ def decode_access_token(token: str) -> dict:
     """
     
     try:
-        payload = jwt.decode(token, settings.JWT_SECRET, algorithms=[ALGORITHM])
-        return payload
+        return jwt.decode(token, settings.JWT_SECRET, algorithms=[ALGORITHM])
     except JWTError:
         return None
+
+def get_current_user(token: str = Depends(oauth2_scheme)) -> ORMUser:
+    payload = decode_access_token(token)
+    if not payload or "sub" not in payload:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials")
+    username = payload["sub"]
+    db = SessionLocal()
+    user = db.query(ORMUser).filter(ORMUser.username == username).first()
+    db.close()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+    return user
+
+def get_current_active_user(current_user: ORMUser = Depends(get_current_user)) -> ORMUser:
+    if not current_user.is_active:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Inactive user")
+    return current_user
